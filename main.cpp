162cd267f079b9266dd64d33eda88428876f79e1
cuibixiong <cuibixiong@gmail.com>
2018-09-03 16:01:18 +0800

    Console class use new to malloc.

--- main.cpp
+++ main.cpp
@@ -4,17 +4,14 @@
 #include <string>
 #include <string.h>
 
-
 int main (int argc, char *argv[])
 {
-    Console console("tclwrapper shell $");
+    Console *console = new Console("tcl shell$> ");
 
     const char *tclWrapper_version = get_version();
-
 #if 0
     if (tclWrapper_version)
         platform.getMsgAdapter().printMessage("tclWrapper Shell Version is %s\n", tclWrapper_version);
 #endif
-
-    console.runShell();
+    console->runShell();
 }
